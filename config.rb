62b55fe308fcc67bf2ba7d9e4ec19219c653adec
bbugguj <bbugguj@jangjaehyuui-MacBook-Pro.local>
2013-05-13 11:18:23 +0900

    install

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/techblog/"
+http_images_path = "/techblog/images"
+http_fonts_path = "/techblog/fonts"
+css_dir = "public/techblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
